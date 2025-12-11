7308ff4d567176a63d54c6e8fd1ee2e3fc22fbaa
ray-avi <rayavinash515@gmail.com>
2025-12-11 13:34:42 +0530

    JavaScript Functions & Arrays Assignment

---
+++ Ass4.js
@@ -0,0 +1,25 @@
+// JavaScript Functions & Arrays Assignment
+
+// comments
+// Introduction:
+// Functions and arrays are building blocks of JavaScript logic.
+
+// Objective:
+
+// Write reusable functions.
+// Work with arrays.
+// Steps:
+
+// Create an array of 5 student names.
+// Write a function to display all names in a list.
+// Add functionality to search for a name.
+// Expected Outcome:
+// A script that dynamically handles and displays array data.
+
+const Students=['Vikas','Pratyush','Avinash','Krishankant','Mern']
+
+for(let i=0; i< Students.length; i++){
+    // console.log("Student name is: " +Students[i])
+        console.log(`Student name is: ${Students[i]}`)
+
+}
