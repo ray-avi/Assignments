35980558bc4b6718b34ee6c13907c125d4843e99
ray-avi <rayavinash515@gmail.com>
2025-12-11 13:18:43 +0530

    Javascript Basics Assignment done

---
+++ Ass3.js
@@ -0,0 +1,23 @@
+// Objective:
+
+// Work with variables, data types, and operators.
+// Write basic logic.
+// Steps:
+
+// Create a JS script that takes two numbers and adds them.
+// Use if/else to check if the sum is even or odd.
+// Display the result on the webpage.
+// Expected Outcome:
+// A basic interactive webpage powered by JavaScript.
+
+let a= 10;
+let b= 23;
+let sum= a+b;
+console.log(sum);
+
+if(sum%2==0){
+    console.log(`${sum} is a even number`);
+}
+else{
+    console.log(`${sum} is a odd number`);
+}
